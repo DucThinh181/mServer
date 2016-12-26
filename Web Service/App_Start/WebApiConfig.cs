10985aa6d4f57fb8e33a6954ea19fed92d34178d
DucThinh181 <h2152181@gmail.com>
2016-12-26 12:50:08 +0700

    change

--- Web Service/App_Start/WebApiConfig.cs
+++ Web Service/App_Start/WebApiConfig.cs
@@ -10,9 +10,9 @@ namespace Web_Service
     {
         public static void Register(HttpConfiguration config)
         {
-            var cors = new EnableCorsAttribute("*", "*", "*");
-            config.EnableCors(cors);
-
+            //var cors = new EnableCorsAttribute("*", "*", "*");
+            //config.EnableCors(cors);
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             //config.EnableCors(new EnableCorsAttribute("http://localhost:1188", "*", "*"));
 
             config.Routes.MapHttpRoute(
